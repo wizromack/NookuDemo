2e683452863f0155ce1908636e8e91d9f4612121
Romack Natividad <romack@wizmediateam.com>
2012-04-13 19:27:40 +0800

    Added hyperlink for edit friend view

--- views/friends/tmpl/default.php
+++ views/friends/tmpl/default.php
@@ -1,60 +1,42 @@
 <?php
 defined('KOOWA') or die('Restricted access');?>
-
+<?= @helper('behavior.mootools');?>
 <script src="media://lib_koowa/js/koowa.js" />
-<?= @helper('behavior.tooltip');?>
 <style src="media://lib_koowa/css/koowa.css" />
 
 <form action="<?= @route();?>" method="GET" class="-koowa-grid">
-	
 	<table class="adminlist">
 		<thead>
 			<tr>
+				<td><?= @helper('grid.checkall');?></td>
+				<td colspan='4'><?= @helper('grid.search');?></td>
+			</tr>
+			<tr>
 				<th width='10'></th>
-				<th>
-					<?= @helper('grid.sort', array('column' => 'Full Name')); ?>
-				</th>
-				<th>
-					<?= @helper('grid.sort', array('column' => 'Email Address')); ?>
-				</th>
-				<th>
-					<?= @helper('grid.sort', array('column' => 'Mobile Phone')); ?>
-				</th>
-				<th>
-					<?= @helper('grid.sort', array('column' => 'Birth Date')); ?>
-				</th>
+				<th><?= @helper('grid.sort', array('column' => 'first_name')); ?></th>				
+				<th><?= @helper('grid.sort', array('column' => 'last_name')); ?></th>
+				<th><?= @helper('grid.sort', array('column' => 'email')); ?></th>
+				<th><?= @helper('grid.sort', array('column' => 'mobile')); ?></th>
+				<th><?= @helper('grid.sort', array('column' => 'birthdate')); ?></th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
-				<td colspan="5">
-					
-				</td>
+				<td colspan="5"><?= @helper('paginator.pagination', array('total' => count($friends)));?></td>
 			</tr>
 		</tfoot>
 		<tbody>
 			<!-- first column (<td></td>) holds the friend(object) -->
 			<? foreach($friends as $friend): ?>
 			<tr>			
-				<td align='center'>
-					<?= @helper('grid.checkbox', array('row' => $friend));?>
-				</td>
-				<td>
-					<?= $friend->first_name . " " .  $friend->last_name; ?>
-				</td>
-				<td>
-					<?= $friend->email; ?>
-				</td>
-				<td>
-					<?= $friend->mobile; ?>
-				</td>
-				<td>
-					<?= $friend->birthdate; ?>
-				</td>
+				<td align='center'><?= @helper('grid.checkbox', array('row' => $friend));?></td>
+				<td><a href="<?= @route("view=friend&id=" . $friend->id);?>"><?= $friend->first_name;?></a></td>
+				<td><a href="<?= @route("view=friend&id=" . $friend->id);?>"><?= $friend->last_name; ?></a></td>
+				<td><?= $friend->email; ?></td>
+				<td><?= $friend->mobile; ?></td>
+				<td><?= $friend->birthdate; ?></td>
 			</tr>
 			<? endforeach;?>
-		</tbody>
-		
+		</tbody>		
 	</table>
-
 </form>
